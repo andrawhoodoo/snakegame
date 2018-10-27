442f7a13d9e9cf8cf0e7d7494dfe5c0b2f4a4b32
andrawhoodoo <43014649+andrawhoodoo@users.noreply.github.com>
2018-10-26 20:50:28 -0400

    Update snakeGame.js

--- snakeGame.js
+++ snakeGame.js
@@ -79,10 +79,10 @@ class Snake {
       this.pos_ = new Point(this.position.posX - steps, this.position.posY);
     }
     else if(this.currentDir_ === "up") {
-      this.pos_ = new Point(this.position.posX, this.position.posY + steps);
+      this.pos_ = new Point(this.position.posX, this.position.posY - steps);
     }
     else if(this.currentDir_ === "down") {
-      this.pos_ = new Point(this.position.posX, this.position.posY - steps);
+      this.pos_ = new Point(this.position.posX, this.position.posY + steps);
     }
   }
   /**
@@ -144,6 +144,10 @@ class WorldModel {
   get height() {
     return this.height_;
   }
+  /**
+   * assigns ownership of a View to the WorldModel
+   * @param {class} newView - new view class you wish to assign
+   */
   set view(newView) {
     if(newView instanceof View) this.view_ = newView;
     else throw new Error("must pass a valid View");
@@ -224,40 +228,39 @@ class AvoidWallsPlayer extends Player {
   /**
    * Create an AvoidWallsPlayer by going to constructor of parent class Player.
    * @param {class} sc - the SnakeController you need to pass to the parent class.
+   * @param {int} scalingFactor - must be the same exact scaling factor integer as provided for the CanvasView class.
    */
-  constructor(sc) {
+  constructor(sc, scalingFactor) {
     super(sc);
+    this.scalingFactor_ = scalingFactor
   }
   /**
-   * Turns the snake to avoid crashing into walls. Turns towards the direction in which there is the most space to move.
+   * Turns the snake to avoid crashing into walls. Turns towards the direction in which there is the most space to move. If no collision with wall impending, does nothing.
    */
   makeTurn() {
     if(this.sc_.snakeDirection === "left" && this.sc_.slitherer_.position.posX == 0) {
-      if(this.sc_.slitherer_.position.posY < (this.sc_.worldHeight - 1)/2) {
+      if(this.sc_.slitherer_.position.posY < ((this.sc_.worldHeight - 1)*(this.scalingFactor_))/2) {
         this.sc_.turnSnakeLeft();
       }
       else this.sc_.turnSnakeRight(); 
     }
-    else if(this.sc_.snakeDirection === "right" && this.sc_.slitherer_.position.posX == (this.sc_.worldWidth - 1)) {
-      if(this.sc_.slitherer_.position.posY < (this.sc_.worldHeight - 1)/2) {
+    else if(this.sc_.snakeDirection === "right" && this.sc_.slitherer_.position.posX == ((this.sc_.worldWidth - 1)*(this.scalingFactor_))) {
+      if(this.sc_.slitherer_.position.posY < ((this.sc_.worldHeight - 1)*(this.scalingFactor_))/2) {
         this.sc_.turnSnakeRight();
       }
       else this.sc_.turnSnakeLeft();
     }
     else if(this.sc_.snakeDirection === "up" && this.sc_.slitherer_.position.posY == 0) {
-      if(this.sc_.slitherer_.position.posX < (this.sc_.worldWidth - 1)/2) {
+      if(this.sc_.slitherer_.position.posX < ((this.sc_.worldWidth - 1)*(this.scalingFactor_))/2) {
         this.sc_.turnSnakeRight();
       }
       else this.sc_.turnSnakeLeft();
     }
-    else if(this.sc_.snakeDirection === "down" && this.sc_.slitherer_.position.posY == (this.sc_.worldHeight - 1)) {
-      if(this.sc_.slitherer_.position.posX < (this.sc_.worldWidth - 1)/2) {
+    else if(this.sc_.snakeDirection === "down" && this.sc_.slitherer_.position.posY == ((this.sc_.worldHeight - 1)*(this.scalingFactor_))) {
+      if(this.sc_.slitherer_.position.posX < ((this.sc_.worldWidth - 1)*(this.scalingFactor_))/2) {
         this.sc_.turnSnakeLeft();
       }
       else this.sc_.turnSnakeRight();
-    }
-    else {
-      pass;
     }
   }
 }
@@ -283,6 +286,7 @@ class CanvasView extends View {
     super();
     this.scalingFactor_ = scalingFactor;
     this.canvas_ = document.createElement("canvas");
+    this.canvas_.id = "game";
     document.body.appendChild(this.canvas_);
     this.context_= this.canvas_.getContext("2d");
   }
@@ -290,11 +294,125 @@ class CanvasView extends View {
    * Determines the width and height of the canvas as well as the size of the snake utilizing the scaling factor property.  Also displays both the canvas and our snake's current position.
    * @param {class} World - the WorldModel class in which our Snake class lives, i.e. the WorldModel we want to display.
    */
-  display(World) {
-    this.canvas_.width = this.scalingFactor_ * World.width;
-    this.canvas_.height = this.scalingFactor_ * World.height;
+  display(world) {
+    this.canvas_.width = this.scalingFactor_ * world.width;
+    this.canvas_.height = this.scalingFactor_ * world.height;
     this.context_.fillStyle = "orange";
-    this.context_.fillRect(5, 10, this.scalingFactor_, this.scalingFactor_);
+    this.context_.fillRect(world.snake.position.posX, world.snake.position.posY, this.scalingFactor_, this.scalingFactor_);
+  }
+}
+
+/** Interface ensuring any type of InputHandler has the madeLeftMove, madeRightMove, resetLeftMove, and resetRightMove methods. */
+class InputHandler {
+  /**
+   * Create a new InputHandler.
+   */
+  constructor() {
+    if(!(this.madeRightMove instanceof Function)) throw new Error("input handler must have madeRightMove method");
+    if(!(this.madeLeftMove instanceof Function)) throw new Error("input handler must have madeLeftMove method");
+    if(!(this.resetRightMove instanceof Function)) throw new Error("input handler must have resetRightMove method");
+    if(!(this.resetLeftMove instanceof Function)) throw new Error("input handler must have resetLeftMove method");
+  }
+}
+
+/** Class representing an event handler for left and right arrow key inputs. Inherits from the InputHandler interface. */
+class LRKeyInputHandler extends InputHandler {
+  /**
+   * Create a new LRKeyInputHandler. Start listening for a keydown event. If event occurs, call eventHandler method.
+   */
+  constructor() {
+    super();
+    this.wasLeftArrowPushed_ = false;
+    this.wasRightArrowPushed_ = false;
+    let eventHandler = event => {
+      if(event.key === "ArrowRight") {
+        this.wasRightArrowPushed_ = true;
+      }
+      else if(event.key === "ArrowLeft") {
+        this.wasLeftArrowPushed_ = true;
+      }
+    }
+    document.getElementById("game").addEventListener("keydown", eventHandler);
+  }
+  madeLeftMove() {
+    return this.wasLeftArrowPushed_;
+  }
+  madeRightMove() {
+    return this.wasRightArrowPushed_;
+  }
+  resetLeftMove() {
+    this.wasLeftArrowPushed_ = false;
+  }
+  resetRightMove() {
+    this.wasRightArrowPushed_ = false;
+  }
+}
+/** Class representing a Human Player of the snake game. */
+class HumanPlayer extends Player {
+  /**
+   * Create a new HumanPlayer.
+   * @param {class} snakeController - the SnakeController class needed to pass to the Player class interface.
+   * @param {class} inputHandler - the type of InputHandler class you wish to use.
+   */
+  constructor(snakeController, inputHandler) {
+    super(snakeController);
+    this.inputHandler_ = inputHandler;
+  }
+  /**
+   * turns the snake left or right using the HumanPlayer's SnakeController based on the InputHandler registering a left arrow keypress or a  right arrow keypress. If no keys are pressed, does nothing.
+   */
+  makeTurn() {
+    if(this.inputHandler_.madeLeftMove() === true) {
+      this.sc_.turnSnakeLeft();
+      this.inputHandler_.resetLeftMove();
+    }
+    else if(this.inputHandler_.madeRightMove() === true) {
+      this.sc_.turnSnakeRight();
+      this.inputHandler_.resetRightMove();
+    }
+  }
+}
+
+/** Class representing a context for the Snake Game: the World Model and Players that are in existence. */
+class GameController {
+  /**
+   * Create a GameController.
+   * @param {class} world - The WorldModel you wish to use.
+   */
+  constructor(world) {
+    this.world_ = world;
+    this.player1_ = null;
+    this.player2_ = null;
+  }
+  /**
+   * Sets player 1 to an instance of a Player Class 'p'.
+   * @param {class} p - the Player class you wish to use.
+   */
+  set player1(p) {
+    if(p instanceof Player) this.player1_ = p;
+    else throw new Error("Must provide a valid player.");
+  }
+  /**
+   * Sets player 2 to an instance of a Player Class 'p'.
+   * @param {class} p - the Player class you wish to use.
+   */
+  set player2(p) {
+    if(p instanceof Player) this.player2_ = p;
+    else throw new Error("Must provide a valid player.");
+  }
+  
+  run() {
+    var lastTime = 0;
+    let updateFrame = milliseconds => {
+      if(!(this.player1_ === null)) this.player1_.makeTurn();
+      if(!(this.player2_ === null))this.player2_.makeTurn();
+      if((milliseconds - lastTime) > 50){
+        this.world_.update(1);
+        lastTime = lastTime + 50;
+      }
+      requestAnimationFrame(updateFrame);
+    }
+    requestAnimationFrame(updateFrame);
   }
 }
 
@@ -302,4 +420,14 @@ let friendlySnake = new Snake();
 let gameTime = new WorldModel(friendlySnake, 42, 42);
 let FooView = new CanvasView(10);
 gameTime.view = FooView;
-gameTime.update(0);
+//gameTime.update(0);
+//gameTime.update(50);
+let SlitherControl = new SnakeController(gameTime, friendlySnake);
+let KeyboardBrain = new LRKeyInputHandler;
+let Hooman = new HumanPlayer(SlitherControl, KeyboardBrain);
+let GameControl = new GameController(gameTime);
+GameControl.player1 = Hooman;
+//let FakeHuman = new AvoidWallsPlayer(SlitherControl, 10);
+//GameControl.player1 = FakeHuman;
+GameControl.run();
+
