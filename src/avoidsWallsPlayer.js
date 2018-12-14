c31238c4f6cc105f5d90ae50c70b916f3eecb66d
andrawhoodoo <43014649+andrawhoodoo@users.noreply.github.com>
2018-12-14 16:37:38 -0500

    Update avoidsWallsPlayer.js

--- src/avoidsWallsPlayer.js
+++ src/avoidsWallsPlayer.js
@@ -3,36 +3,34 @@ class AvoidWallsPlayer extends Player {
   /**
    * Create an AvoidWallsPlayer by going to constructor of parent class Player.
    * @param {class} sc - the SnakeController you need to pass to the parent class.
-   * @param {int} scalingFactor - must be the same exact scaling factor integer as provided for the CanvasView class.
    */
-  constructor(sc, scalingFactor) {
-    super(sc);
-    this.scalingFactor_ = scalingFactor
+  constructor(sc) {
+    super(sc)
   }
   /**
    * Turns the snake to avoid crashing into walls. Turns towards the direction in which there is the most space to move. If no collision with wall impending, does nothing.
    */
-  makeTurn() {
+    makeTurn() {
     if(this.sc_.snakeDirection === "left" && this.sc_.slitherer_.position.posX == 1) {
-      if(this.sc_.slitherer_.position.posY < ((this.sc_.worldHeight - 1)*(this.scalingFactor_))/2) {
+      if(this.sc_.slitherer_.position.posY < ((this.sc_.worldHeight - 1)/2)) {
         this.sc_.turnSnakeLeft();
       }
       else this.sc_.turnSnakeRight(); 
     }
-    else if(this.sc_.snakeDirection === "right" && this.sc_.slitherer_.position.posX == ((this.sc_.worldWidth - 2)*(this.scalingFactor_))) {
-      if(this.sc_.slitherer_.position.posY < ((this.sc_.worldHeight - 1)*(this.scalingFactor_))/2) {
+    else if(this.sc_.snakeDirection === "right" && this.sc_.slitherer_.position.posX == (this.sc_.worldWidth - 2)) {
+      if(this.sc_.slitherer_.position.posY < ((this.sc_.worldHeight - 1)/2)) {
         this.sc_.turnSnakeRight();
       }
       else this.sc_.turnSnakeLeft();
     }
     else if(this.sc_.snakeDirection === "up" && this.sc_.slitherer_.position.posY == 1) {
-      if(this.sc_.slitherer_.position.posX < ((this.sc_.worldWidth - 1)*(this.scalingFactor_))/2) {
+      if(this.sc_.slitherer_.position.posX < ((this.sc_.worldWidth - 1)/2)) {
         this.sc_.turnSnakeRight();
       }
       else this.sc_.turnSnakeLeft();
     }
-    else if(this.sc_.snakeDirection === "down" && this.sc_.slitherer_.position.posY == ((this.sc_.worldHeight - 2)*(this.scalingFactor_))) {
-      if(this.sc_.slitherer_.position.posX < ((this.sc_.worldWidth - 1)*(this.scalingFactor_))/2) {
+    else if(this.sc_.snakeDirection === "down" && this.sc_.slitherer_.position.posY == (this.sc_.worldHeight - 2)) {
+      if(this.sc_.slitherer_.position.posX < ((this.sc_.worldWidth - 1)/2)) {
         this.sc_.turnSnakeLeft();
       }
       else this.sc_.turnSnakeRight();
